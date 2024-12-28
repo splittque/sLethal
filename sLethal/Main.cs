afadfffd885dd3857f8341c096217df6c3986f98
splittque <140718354+splittque@users.noreply.github.com>
2024-12-28 13:31:04 +0500

    version 0.0.1

--- sLethal/Main.cs
+++ sLethal/Main.cs
@@ -11,7 +11,7 @@ namespace sLethal
     {
         private const string modID = "me.splitque.lethal";
         private const string modName = "sLethal";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "0.0.1";
 
         private Harmony harmony;
         private TerminalModRegistry commands;
